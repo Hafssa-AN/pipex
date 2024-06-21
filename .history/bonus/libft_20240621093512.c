6e2ee118924845a910455262e93b69e3162310a3
Hafssa Anebarou <hanebaro@c2r4p8.1337.ma>
2024-06-21 09:47:00 +0100

    chi 7aja

---
+++ .history/bonus/libft_20240621093512.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanebaro <hanebaro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/20 16:14:57 by hanebaro          #+#    #+#             */
+/*   Updated: 2024/06/21 09:35:12 by hanebaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/pipex_bonus.h"
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int	ft_strchr(char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (i <= ft_strlen(s))
+	{
+		if (s[i] == (char)c)
+			return (i + 1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strjoin(char *s1, char *s2, int x)
+{
+	size_t	i;
+	char	*str;
+	size_t	len1;
+	size_t	len2;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	if ((!s1 && !s2) || !s2)
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!str)
+		return (NULL);
+	i = -1;
+	while (s1 && s1[(++i)])
+		str[i] = s1[i];
+	if (!s1)
+		i = 0;
+	while (s2[(i++) - len1])
+		str[i - 1] = s2[i - len1 - 1];
+	str[i - 1] = '\0';
+	if(x == 1)
+		free (s1);
+	return (str);
+}
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+	char *str;
+	printf("s 1 = [%s]")
+	str = ft_strjoin((char *)s2,"\n",0);
+	i = 0;
+	while ((s1[i] || str[i]))
+	{
+		if ((unsigned char)s1[i] > (unsigned char)s2[i])
+			return (1);
+		if ((unsigned char)s1[i] < (unsigned char)s2[i])
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while ((s1[i] || s2[i]) && i < n)
+	{
+		if ((unsigned char)s1[i] > (unsigned char)s2[i])
+			return (1);
+		if ((unsigned char)s1[i] < (unsigned char)s2[i])
+			return (-1);
+		i++;
+	}
+	return (0);
+}
---
+++ .history/bonus/main_bonus_20240621075929.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanebaro <hanebaro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/20 07:29:54 by hanebaro          #+#    #+#             */
+/*   Updated: 2024/06/21 07:59:29 by hanebaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "../includes/pipex_bonus.h"
+
+void pipes(char *cmd, char **envp)
+{
+    int p[2];
+    pid_t pid;
+    char **var;
+
+    if(pipe(p) == -1)
+        my_exit();
+    pid = fork();
+    if(pid == -1)
+        my_exit();
+    if(pid == 0)
+    {
+        dup2(p[1], STDOUT_FILENO);
+        close(p[0]);
+        var = some_var(envp, cmd);
+        if (execve(var[0], &var[1], envp) == -1)
+		    my_exit();
+    }
+    else
+    {
+        dup2(p[0],STDIN_FILENO);
+        close(p[0]);
+        close(p[1]);
+    }
+}
+void end(int fd,char *cmd, char **envp)
+{
+    int pid;
+    char **var;
+
+    pid = fork();
+	if (pid == -1)
+		my_exit();
+	if (pid == 0)
+	{
+		dup2(fd, STDOUT_FILENO);
+		var = some_var(envp, cmd);
+        if (execve(var[0], &var[1], envp) == -1)
+		    my_exit();
+	}
+}
+#include<string.h>
+int main(int argc, char **argv, char *envp[])
+{
+    int fd;
+    int i;
+
+    fd = 0;
+    i = 2;
+    
+    if (argc < 5)
+        return(write (2,"more arguments\n", 15));
+    if(argv[1] && !ft_strcmp("here_doc",argv[1]))//check return value for this function
+    {
+        here_doc(argc, argv);
+        i = 3;
+    }
+    if(i == 2)
+    {
+        fd = open(argv[1], O_RDONLY);
+	    if (fd == -1)  
+            my_exit();    
+        dup2(fd, STDIN_FILENO);
+    }
+    
+    while(i < argc - 2)
+        pipes(argv[i++], envp);
+    
+    if(i == argc - 2)
+    {
+        fd = open(argv[argc - 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
+        if (fd == -1)
+        {
+            printf("in argc - 1\n");
+            exit(1);
+            my_exit();
+        }
+		    
+    }
+    
+    end(fd, argv[argc - 2], envp);
+    while ((argc--) - 3 > 0)//verifie the condition
+        wait(NULL);//if i wait more than number of child process exist , it s probleme or not ??
+    close(fd);
+    check_leaks();
+    return(0);
+}
